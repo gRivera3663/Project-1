512d0c77f816e3837a8e68bdfa5f94d4b7bab579
Gabriel Rivera <gabrielrivera457@gmail.com>
2017-02-23 18:00:55 -0800

    Baby class finished

--- Baby.h
+++ Baby.h
@@ -6,24 +6,27 @@ using namespace std;
 // class that contains information related to a single birth or baby name
 class Baby {
 public:
-	Baby() {  // default constructor
-	};
-
-	Baby(string s, int w) { // constructor
-		// TO BE COMPLETED
-	}
-
-	// a "getter" method
-	int getWeight() {
-		return -1; // TO BE COMPLETED
-	}
-
-	// a "getter" method
-	string getName() {
-		return "COMPLETE ME"; // TO BE COMPLETED
-	}
-
+    Baby() {}; // default constructor
+    
+    Baby(string s, int w) // constructor
+    {
+        name = s;
+        weight = w;
+    }
+    
+    // a "getter" method
+    int getWeight()
+    {
+        return weight;
+    }
+    
+    // a "getter" method
+    string getName()
+    {
+        return name;
+    }
+    
 private:
-	string name;
-	int weight;
+    string name;
+    int weight;
 };
